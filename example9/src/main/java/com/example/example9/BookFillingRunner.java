c260cb7da338b2e19ff9ba1ad42202bf55bc486d
User <User@DESKTOP-5D1LJ47>
2021-05-21 19:37:06 +0200

    random faker

--- example9/src/main/java/com/example/example9/BookFillingRunner.java
+++ example9/src/main/java/com/example/example9/BookFillingRunner.java
@@ -1,5 +1,7 @@
 package com.example.example9;
 
+import java.util.Random;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,28 +18,68 @@ public class BookFillingRunner implements CommandLineRunner {
 	@Autowired
 	private BookRepository bookRepository;
 
-	@Override
+	
 	public void run(String... args) throws Exception {
-		
-		//Faker faker = new Faker();
 
-		//String name = faker.book().title();  
-		//String firstName = faker.book().genre();
-		
-		
-		
-		bookRepository.deleteAll();
-		
+		String alphabetChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!·$%&/()=?¿?=)()/*-+^*Ç¨_:;;:_+/+/";
 
-		bookRepository.save(new Book("Programming Java", "SGRWE-456WSFG-D", 125));
-		bookRepository.save(new Book("Programming JS", "SGRWE-456WSFG-D", 239));
-		bookRepository.save(new Book("Programming PHP", "ERF-3333SFG-D", 456));
-		bookRepository.save(new Book("Programming react", "SGRWE-456WSFG-D", 125));
-		bookRepository.save(new Book("Programming angualar", "SGRWE-456WSFG-D", 239));
-		bookRepository.save(new Book("Programming unity", "ERF-3333SFG-D", 456));
+		//int i = 0;
+		char stringRandom;
+		/*
+		 * while (i < 10) { char charRandom =
+		 * alphabetChars.charAt(createIntRandom(alphabetChars.length())); 
+		 * stringRandom * += stringRandom + charRandom;
+		 * 
+		 * }
+		 */
 		
+		Faker faker = new Faker();
+		Random rand = new Random();
+		int max = 1525;
+		int count = 1;
+
+		//bookRepository.deleteAll();
+		
+		if (bookRepository.count()<0) {
+
+		
+					while (count < 50) {
+			
+						stringRandom = alphabetChars.charAt(createIntRandom(alphabetChars.length()));
+						int intRandom = rand.nextInt(max);
+						boolean randomPublished;
+						if ((intRandom % 2) == 0) {
+							randomPublished = true;
+						} else {
+							randomPublished = false;
+						}
+			
+						bookRepository.save(new Book(
+								faker.book().title(),
+								faker.book().publisher(),
+								String.valueOf((intRandom + 5) * count * 6) + stringRandom ,
+								faker.book().genre(),
+								intRandom + 10,
+								randomPublished));
+			
+						count++;
+					}
+		
+		}
+
 		bookRepository.findAll().forEach((book) -> {
 			logger.info("{}", book);
 		});
 	}
+
+	public static int createIntRandom(int top) {
+
+		Random rand = new Random();
+
+		// Generate random integers in range 0 to top, if top=10 random 0 to 9
+		int intRandom = rand.nextInt(top);
+		return intRandom;
+
+	}
+
 }
--- example9/src/main/java/com/example/example9/Example9Application.java
+++ example9/src/main/java/com/example/example9/Example9Application.java
@@ -3,8 +3,9 @@ package com.example.example9;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-
+@EnableJpaAuditing
 @SpringBootApplication
 public class Example9Application {
 
