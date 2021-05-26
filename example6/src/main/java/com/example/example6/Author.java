6216040d1192ce6b95a1d24a9438129b65fc14ad
User <User@DESKTOP-5D1LJ47>
2021-05-26 19:52:24 +0200

    mongodb embeded

---
+++ example6/src/main/java/com/example/example6/Author.java
@@ -0,0 +1,72 @@
+package com.example.example6;
+
+import java.util.List;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "authors")
+public class Author {
+
+	@Id
+	private String id;
+	private String name;
+	private String surname;
+	private int age;
+	
+	private List<Book> books;
+	
+	
+	public Author(String name, String surname, int age, List<Book> books) {
+		super();
+	
+		this.name = name;
+		this.surname = surname;
+		this.age = age;
+		this.books = books;
+	}
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getSurname() {
+		return surname;
+	}
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public List<Book> getBooks() {
+		return books;
+	}
+	public void setBooks(List<Book> books) {
+		this.books = books;
+	}
+	@Override
+	public String toString() {
+		return "Author [id=" + id + ", name=" + name + ", surname=" + surname + ", age=" + age + ", books=" + books
+				+ "]";
+	}
+	
+	
+	
+	
+	
+	
+	
+
+}
---
+++ example6/src/main/java/com/example/example6/EmbededWrapper.java
@@ -0,0 +1,27 @@
+package com.example.example6;
+
+import java.util.List;
+
+public class EmbededWrapper {
+	
+	List<Book> books;
+	Author author;
+	
+	
+	public List<Book> getBooks() {
+		return books;
+	}
+	public void setBooks(List<Book> books) {
+		this.books = books;
+	}
+	public Author getAuthor() {
+		return author;
+	}
+	public void setAuthor(Author author) {
+		this.author = author;
+	}
+	
+	
+	
+
+}
