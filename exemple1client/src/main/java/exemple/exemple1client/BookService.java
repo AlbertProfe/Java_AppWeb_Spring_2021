ca710f65dd62137da5f8f52ad18274fd329551d0
User <User@DESKTOP-5D1LJ47>
2021-06-05 08:56:29 +0200

    commit one

--- exemple1client/src/main/java/exemple/exemple1client/BookService.java
+++ exemple1client/src/main/java/exemple/exemple1client/BookService.java
@@ -8,28 +8,59 @@ import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
-
 @Service
 public class BookService {
-	
-	
-	RestTemplate template = new RestTemplate();
-	
-	
-	public Iterable<Book> queryBooks() {
-		
-		ResponseEntity<List<Book>> response = template.exchange(
-				"http://localhost:8081/webapi/allbooks",
-				HttpMethod.GET,
-				null,
-				new ParameterizedTypeReference<List<Book>>(){});
-		
+
+	RestTemplate restTemplate = new RestTemplate();
+
+	public List<Book> queryBooks() {
+
+		ResponseEntity<List<Book>> response = restTemplate.exchange(
+				"http://localhost:8081/webapi/allBooks",
+				HttpMethod.GET, 
+				null, 
+				new ParameterizedTypeReference<List<Book>>() {
+				});
+
 		List<Book> books = response.getBody();
-		
-		
+
 		return books;
+
+	}
+
+	public void queryDeleteBook(String titleToDelete) {
+
+		restTemplate.delete("http://localhost:8081/webapi/deleteBook" + "/{titleToDelete}",
+				titleToDelete);
+	}
+
+	public void queryAddBook(Book book) {
+
+		restTemplate.postForObject("http://localhost:8081/webapi/addBook",
+				book,
+				Book.class);
+
+	}
+
+	public void queryUpdateBook(String title, Book book) {
 		
-		
+		restTemplate.put("http://localhost:8081/webapi/updateBook" + "/{title}",
+				book,
+				title
+				);
+
+	}
+
+	public Book queryBook(String titleToGet) {
+
+		ResponseEntity<Book> response = restTemplate.getForEntity(
+				"http://localhost:8081/webapi/getBook" + "/{titleToGet}",
+				Book.class,
+				titleToGet);
+
+		Book book = response.getBody();
+
+		return book;
 	}
 
 }
